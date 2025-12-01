26ebb9e64b0b88bd0f9f92ab8aff21134bca9fb1
DJ <DJdj@Darshans-MacBook-Air.local>
2025-12-02 00:19:01 +0530

    longestSubStringWithourRepeatingCharacter

---
+++ LeetCode/src/main/java/org/example/LongestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,43 @@
+package org.example.LongestSubstringWithoutRepeatingCharacters;
+
+import java.util.HashSet;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+    public static int lengthOfLongestSubstring(String s) {
+        HashSet<Character> set = new HashSet<>();
+        int ans = 0;
+        int p1 = 0;
+        int p2 = 0;
+
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            if(!set.contains(ch)){
+                //increment p1 for unique substring
+                p1++;
+                set.add(ch);
+                //ans is max of ans till now, and current i - second pointer + 1
+                ans = Math.max(ans, i - p2 +1);
+            }else{
+                //when we find duplicate, we start reducing subarray size from second pointer
+                //till we found the duplicate array
+                // this will help us to maintain size of unique array at given index between
+                // given window.
+                while(s.charAt(p2) != ch){
+                    set.remove(s.charAt(p2));
+                    p2++;
+                }
+                set.remove(s.charAt(p2));
+                p2++;
+                set.add(ch);
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        String s = "abcasdbadasdcjbas";
+
+        System.out.println(lengthOfLongestSubstring(s));
+    }
+}
